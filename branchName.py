c871e09356f8bb392951b2024582c46bc9da464f
mohamed a sattar <msabdo54@gmail.com>
2019-04-08 23:00:21 +0200

    branch name file

---
+++ branchName.py
@@ -0,0 +1,11 @@
+def name():
+    print("new branch ")
+    print("new file in new branch")
+
+def main():
+    name()
+
+if __name__ == "__main__":
+    print("------------------------------------")
+    main()
+    print("----------------------------------------")
