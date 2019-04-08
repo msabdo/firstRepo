d5fc38c7679732f4f9e311ef99c9dfd653467cfa
mohamed a sattar <msabdo54@gmail.com>
2019-04-08 21:16:31 +0200

    added the hello file

---
+++ hello.py
@@ -0,0 +1,11 @@
+
+def print_hello():
+   print("Hi pepole")
+
+def main():
+    print_hello()       
+    
+    
+if __name__=="__main__":
+    print("--------------------------------")
+    main()
